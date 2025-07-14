1d2af866ef5687c3974d28df3fa1d79e70a04798
gb090200 <giribala0902@gmail.com>
2025-07-14 23:19:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ def send_pdf():
     return "PDFs sent", 200
 
 if __name__ == '__main__':
-    app.run import os
+     import os
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
 
