f96987582c062f455481d3639900507e7b9cc01f
gb090200 <giribala0902@gmail.com>
2025-07-15 09:42:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -93,4 +93,5 @@ def upi_webhook():
     return {"success": False, "error": "No matching txn"}, 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
